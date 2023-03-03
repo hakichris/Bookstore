5d63251ca6868c1b5344572e23c122fbc5534b6d
Chris <hakizachrist1@gmail.com>
2023-03-03 13:14:54 +0200

    Update books

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,20 +1,28 @@
-const Books = (title, author) => (
- <div>
- <ul>
-   <div className="book flexRow"> <li >title: Book1 Author: CHRIS</li> 
-   <button className="remove">remove</button>
-   </div>
- </ul>
-  
+const Books = ({ title, author }) => (
   <div>
-    <form className="Form flexColumn">
-        <input type="text" id="author" placeholder="Author"/>
-        <input type="text" id="title" placeholder="Book Title"/>
-        <button type="submit">Submit</button>
-    </form>
-  </div>
- </div>
- 
-)
+    <ul>
+      <div className="book flexRow">
+        {' '}
+        <li>
+          title:
+          {title}
+          {' '}
+          author:
+          {author}
+        </li>
+        <button type="submit" className="remove">remove</button>
+      </div>
+    </ul>
 
-export default Books; 
+    <div>
+      <form className="Form flexColumn">
+        <input type="text" id="author" placeholder="Author" />
+        <input type="text" id="title" placeholder="Book Title" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+
+);
+
+export default Books;
