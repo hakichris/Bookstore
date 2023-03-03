4501e39411a91b32dc9ff45088f3034c238bc730
Chris <hakizachrist1@gmail.com>
2023-03-03 14:06:55 +0200

    Update book

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -13,14 +13,6 @@ const Books = ({ title, author }) => (
         <button type="submit" className="remove">remove</button>
       </div>
     </ul>
-
-    <div>
-      <form className="Form flexColumn">
-        <input type="text" id="author" placeholder="Author" />
-        <input type="text" id="title" placeholder="Book Title" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
   </div>
 
 );
