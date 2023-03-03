533de3663b9e1feda5dd2accb8055ee36b9966df
Chris <hakizachrist1@gmail.com>
2023-03-03 10:51:38 +0200

    Update book

---
+++ src/components/books.jsx
@@ -0,0 +1,11 @@
+const Books = () => {
+ <ul>
+    <li>title: Book1 Author: CHRIS</li>
+    
+    <li>title: Book1 Author: CHRIS</li>
+  
+    <li>title: Book1 Author: CHRIS</li>
+ </ul>
+}
+
+export default Books; 
