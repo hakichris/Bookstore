ffabe0d1f120eedc75f3a8047962db0b509b3b95
Chris <hakizachrist1@gmail.com>
2023-03-03 12:37:48 +0200

    Update App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Books from './components/books';
+import './App.css'
 import Categories from './components/categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <nav>
-        <ul>
-          <li><Link to="/books">Books</Link></li>
-          <div className="verticalLine" />
-          <li><Link to="/categories">Categories</Link></li>
-        </ul>
-      </nav>
+      < Navbar />
       <Routes>
-        <Route path="/books" element={<Books />} />
+        <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
     </>
