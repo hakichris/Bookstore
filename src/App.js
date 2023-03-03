8d113c8a00c7ce58755c05ddf9f8ac6ceb528d7c
Chris <hakizachrist1@gmail.com>
2023-03-03 13:14:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Books from './components/books';
-import './App.css'
+import './App.css';
 import Categories from './components/categories';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      < Navbar />
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Books />} />
+        <Route path="/" element={<Books title="Book1" author="Chris" />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
