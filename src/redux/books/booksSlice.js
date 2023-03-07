8c57ccfca5a385ab43dc90c26842a8b187c7e8c8
Chris <hakizachrist1@gmail.com>
2023-03-07 17:11:27 +0200

    Update bookslice

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  book : [],
+}
+
+export const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    Addbook: (state,action) => {
+      state.push({
+        id: action.payload.id,
+        title: action.payload.title,
+        author: action.payload.author,
+        category: action.payload.category
+      })
+    },
+    Removebook: (state, action) => {
+        state.book = state.book.filter((item) => item.item_id !== action.payload )
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { Addbook, Removebook } = bookSlice.actions
+
+export default bookSlice.reducer
