dce6100a5b2273985b9dcb95cdcb182729cf0ad3
Chris <hakizachrist1@gmail.com>
2023-03-09 17:09:46 +0200

    Update categories

--- src/pages/categoriespage.jsx
+++ src/pages/categoriespage.jsx
@@ -1,7 +1,18 @@
-const Categories = () => (
+import { useDispatch, useSelector } from 'react-redux';
+import { Checkstatus } from '../redux/categories/categoriesSlice';
 
-  <button type="button">Check Status</button>
+const Categories = () => {
+  const status = useSelector((state) => state.categoriesReducer.status)
+  const dispatch = useDispatch();
+  console.log(status, "check status")
+  return (
+  <>
+    <div>{status}</div>
+    <button type="button" onClick={() => dispatch(Checkstatus())}>Check Status</button>
+  </>
+  )
+  
 
-);
+};
 
 export default Categories;
