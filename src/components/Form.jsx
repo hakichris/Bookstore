0251cad46a3b034c4c896b10a015b86c6e58abd1
Chris <hakizachrist1@gmail.com>
2023-03-09 14:42:38 +0200

    Update form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,35 @@
-const Form = () => (
-  <div>
-    <form className="Form flexColumn">
-      <input type="text" id="author" placeholder="Author" />
-      <input type="text" id="title" placeholder="Book Title" />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Addbook } from '../redux/books/booksSlice';
 
+const initials = {
+  title: '',
+  author: '',
+  category: '',
+};
+
+const Form = () => {
+  const [state, setState] = useState(initials);
+  const dispatch = useDispatch();
+  const Array = useSelector((state) => state.bookReducer.books);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({ ...prev, [name]: value, item_id: `item${Array.length + 1}` }));
+  };
+
+  const handleSubmit = () => {
+    dispatch(Addbook(state));
+  };
+
+  return (
+    <div>
+      <form className="Form flexColumn" method="post">
+        <input type="text" id="author" name="author" placeholder="Author" onClick={handleChange} required />
+        <input type="text" id="title" name="bookTitle" placeholder="Book Title" onClick={handleChange} required />
+        <button type="button" onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  );
+};
 export default Form;
--- src/pages/bookpage.jsx
+++ src/pages/bookpage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from '../components/books';
 import Form from '../components/Form';
