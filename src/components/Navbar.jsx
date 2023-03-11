a2b7486cbeef5e11fff8e30c8cd6c5ce68734c3b
Chris <hakizachrist1@gmail.com>
2023-03-11 14:11:27 +0200

    Update App.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,22 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="panel-bg">
-    <div>
-      <span className="Bookstore-CMS Text-Style-3">Bookstore CMS</span>
-    </div>
+    <div className="firstpart">
+      <div>
+        <span className="Bookstore-CMS Text-Style-3">Bookstore CMS</span>
+      </div>
 
-    <div className="BOOKS">
-      <Link to="/" className="navlink">BOOKS</Link>
-    </div>
+      <div className="BOOKS">
+        <Link to="/" className="navlink">
+          BOOKS
+        </Link>
+      </div>
 
-    <div className="verticalLine" />
-
-    <div className="categories">
-      {' '}
-      <Link to="/categories" className="navlink">CATEGORIES</Link>
+      <div className="categories">
+        <Link to="/categories" className="navlink">
+          CATEGORIES
+        </Link>
+      </div>
     </div>
     <div className="user">
       <i className="fa-solid fa-user" />
